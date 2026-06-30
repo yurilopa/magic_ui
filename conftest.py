8a594f8b1f05d50c8053f8fda6cfbbea7928b537
YuryiLo <uryi.84@mail.ru>
2026-06-30 13:33:11 +0700

    Update config file

--- conftest.py
+++ conftest.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import pytest
-from time import sleep
+# from time import sleep  # для запуска in pycharm
 from pages.cart_page import CartPage
 from pages.cart_page_text import CartPageText
 from pages.desk1_page_visibility import Desk1PageVisibility
@@ -14,8 +15,13 @@ from pages.all_page_text import AllPageText
 '''**Фикстура `driver`** создает браузер Chrome'''
 @pytest.fixture()
 def driver():
-    chrome_driver = webdriver.Chrome()
-    sleep(3)
+    options = Options()  # для запуска на сервере
+    options.add_argument("--headless")  # для запуска на сервере
+    options.add_argument("--no-sandbox")  # для запуска на сервере
+    options.add_argument("--disable-dev-shm-usage")  # для запуска на сервере
+    chrome_driver = webdriver.Chrome(options=options)  # для запуска на сервере
+    # chrome_driver = webdriver.Chrome()  # для запуска in pycharm
+    # sleep(3)  # для запуска in pycharm
     yield chrome_driver  # ← Передаем драйвер в тест
     chrome_driver.delete_all_cookies()
     chrome_driver.quit()  # После теста закрываем браузер
